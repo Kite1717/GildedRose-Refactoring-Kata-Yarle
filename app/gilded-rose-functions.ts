90b583f28c3078192b16ecc5040ce2c74c0b2ead
Mustafa Yilmaz <mustafa.yilmaz@kobil.com>
2022-07-28 01:30:52 +0300

    Add : updateDecreaseQualityItem function, Refactor : updatequality functions

--- app/gilded-rose-functions.ts
+++ app/gilded-rose-functions.ts
@@ -7,44 +7,44 @@ const increaseQuality = (quality: number) => Math.min(MAX_QUALITY, quality + 1);
 const decreaseQuality = (quality: number) => Math.max(MIN_QUALITY, quality + 1);
 
 export const updateABrie = (item: Item) => {
-  item.quality = increaseQuality(item.quality);
-  item.sellIn -= 1;
-  item.quality = item.sellIn < 0 ? increaseQuality(item.quality) : item.quality;
+    item.quality = increaseQuality(item.quality);
+    item.sellIn -= 1;
+    item.quality = item.sellIn < 0 ? increaseQuality(item.quality) : item.quality;
 };
 
 export const updateBPass = (item: Item) => {
-  if (item.sellIn === 0) {
-    item.quality = 0;
-  } else {
-    item.quality = increaseQuality(item.quality);
-    item.quality =
-      item.sellIn < 11 ? increaseQuality(item.quality) : item.quality;
-    item.quality =
-      item.sellIn < 6 ? increaseQuality(item.quality) : item.quality;
-  }
-  item.sellIn -= 1;
+    if (item.sellIn === 0) {
+        item.quality = 0;
+    } else {
+        item.quality = increaseQuality(item.quality);
+        item.quality =
+            item.sellIn < 11 ? increaseQuality(item.quality) : item.quality;
+        item.quality =
+            item.sellIn < 6 ? increaseQuality(item.quality) : item.quality;
+    }
+    item.sellIn -= 1;
 };
 
-export const updateSHand = (item: Item) => {};
+export const updateSHand = (item: Item) => { };
 
 export const updateDefault = (item: Item) => {
-  item.quality = decreaseQuality(item.quality);
-  item.quality =
-    item.sellIn <= 0 ? decreaseQuality(item.quality) : item.quality;
-  item.sellIn -= 1;
+    item = updateDecreaseQualityItem(item);
+    item.sellIn -= 1;
 };
 
 export const updateConj = (item: Item) => {
-  if (item.sellIn === 5) {
-    item.quality -= 3;
-  } else {
+    if (item.sellIn === 5) {
+        item.quality -= 3;
+    } else {
+        item = updateDecreaseQualityItem(item);
+        item = updateDecreaseQualityItem(item);
+    }
+    item.sellIn -= 1;
+};
+
+const updateDecreaseQualityItem = (item: Item): Item => {
     item.quality = decreaseQuality(item.quality);
     item.quality =
-      item.sellIn <= 0 ? decreaseQuality(item.quality) : item.quality;
-      
-    item.quality = decreaseQuality(item.quality);
-    item.quality =
-      item.sellIn <= 0 ? decreaseQuality(item.quality) : item.quality;
-  }
-  item.sellIn -= 1;
+        item.sellIn <= 0 ? decreaseQuality(item.quality) : item.quality;
+    return item;
 };
