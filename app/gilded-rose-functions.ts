ca82a7cbe1c67378d58909f1b3176792d456a002
Mustafa Yilmaz <mustafa.yilmaz@kobil.com>
2022-07-28 01:21:15 +0300

    Update : updateBPass function

--- app/gilded-rose-functions.ts
+++ app/gilded-rose-functions.ts
@@ -6,32 +6,27 @@ const MIN_QUALITY = 0;
 const increaseQuality = (quality: number) => Math.min(MAX_QUALITY, quality + 1);
 const decreaseQuality = (quality: number) => Math.max(MIN_QUALITY, quality + 1);
 
-
-
 export const updateABrie = (item: Item) => {
     item.quality = increaseQuality(item.quality);
     item.sellIn -= 1;
     item.quality = item.sellIn < 0 ? increaseQuality(item.quality) : item.quality;
-}
+};
 
 export const updateBPass = (item: Item) => {
-    if (item.quality < 50) {
-        item.quality = item.quality + 1
-        if (item.sellIn < 11 && item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-        if (item.sellIn < 6 && item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
+    if (item.sellIn === 0) {
+        item.quality = 0;
+    } else {
+        item.quality = increaseQuality(item.quality);
+        item.quality =
+            item.sellIn < 11 ? increaseQuality(item.quality) : item.quality;
+        item.quality =
+            item.sellIn < 6 ? increaseQuality(item.quality) : item.quality;
     }
-    item.sellIn = item.sellIn - 1;
-    if (item.sellIn < 0) {
-        item.quality = item.quality - item.quality;
-    }
-}
+    item.sellIn -= 1;
 
-export const updateSHand = (item: Item) => {
-}
+};
+
+export const updateSHand = (item: Item) => { };
 
 export const updateDefault = (item: Item) => {
     if (item.quality > 0) {
@@ -41,4 +36,4 @@ export const updateDefault = (item: Item) => {
     if (item.sellIn < 0 && item.quality > 0) {
         item.quality = item.quality - 1;
     }
-}
+};
