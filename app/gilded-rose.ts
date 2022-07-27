61004d1858eea881804021c58d4bc54a282af172
Mustafa Yilmaz <mustafa.yilmaz@kobil.com>
2022-07-27 23:06:01 +0300

    Add : switch case statement for conditons

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -15,6 +15,68 @@ const ItemTypes = {
   S_HAND: 'Sulfuras, Hand of Ragnaros'
 }
 
+const updateABrie = (item) => {
+  if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+    if (item.quality > 0) {
+      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        item.quality = item.quality - 1
+      }
+    }
+  } else {
+    if (item.quality < 50) {
+      item.quality = item.quality + 1
+      if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.sellIn < 11) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1
+          }
+        }
+        if (item.sellIn < 6) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1
+          }
+        }
+      }
+    }
+  }
+  if (item.name != 'Sulfuras, Hand of Ragnaros') {
+    item.sellIn = item.sellIn - 1;
+  }
+  if (item.sellIn < 0) {
+    if (item.name != 'Aged Brie') {
+      if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.quality > 0) {
+          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            item.quality = item.quality - 1
+          }
+        }
+      } else {
+        item.quality = item.quality - item.quality
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1
+      }
+    }
+  }
+
+}
+
+const updateBPass = (item) => {
+
+
+}
+
+const updateSHand = (item) => {
+
+
+}
+
+const updateDefault = (item) => {
+
+
+}
+
 export class GildedRose {
   items: Array<Item>;
 
@@ -24,49 +86,22 @@ export class GildedRose {
 
   updateQuality() {
     for (const item of this.items) {
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1
-          }
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-          }
-        }
+
+      switch (item.name) {
+        case ItemTypes.A_BRIE:
+          updateABrie(item);
+          continue;
+        case ItemTypes.B_PASS:
+          updateBPass(item);
+          continue;
+        case ItemTypes.S_HAND:
+          updateSHand(item);
+          continue;
+        default:
+          updateDefault(item);
+          continue;
       }
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.sellIn = item.sellIn - 1;
-      }
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality = item.quality - 1
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
-        }
-      }
+
     }
 
     return this.items;
