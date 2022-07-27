a4bf096c73e8aebb19bf3d20424c23bb1c9b8b2a
Mustafa Yilmaz <mustafa.yilmaz@kobil.com>
2022-07-28 00:55:59 +0300

    Refactor : update functions

--- app/gilded-rose-functions.ts
+++ app/gilded-rose-functions.ts
@@ -1,8 +1,48 @@
+import { Item } from "./item";
 
 const MAX_QUALITY = 50;
 const MIN_QUALITY = 0;
 
-const increaseQuality = (quality: number) => Math.min(MAX_QUALITY, quality + 1)
-const decreaseQuality = (quality: number) => Math.max(MIN_QUALITY, quality + 1)
+const increaseQuality = (quality: number) => Math.min(MAX_QUALITY, quality + 1);
+const decreaseQuality = (quality: number) => Math.max(MIN_QUALITY, quality + 1);
 
 
+
+export const updateABrie = (item: Item) => {
+    if (item.quality < 50) {
+        item.quality = item.quality + 1;
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0 && item.quality < 50) {
+        item.quality = item.quality + 1;
+    }
+}
+
+export const updateBPass = (item: Item) => {
+    if (item.quality < 50) {
+        item.quality = item.quality + 1
+        if (item.sellIn < 11 && item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+        if (item.sellIn < 6 && item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0) {
+        item.quality = item.quality - item.quality;
+    }
+}
+
+export const updateSHand = (item: Item) => {
+}
+
+export const updateDefault = (item: Item) => {
+    if (item.quality > 0) {
+        item.quality = item.quality - 1;
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < 0 && item.quality > 0) {
+        item.quality = item.quality - 1;
+    }
+}
--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,3 +1,4 @@
+import { updateABrie, updateBPass, updateSHand, updateDefault } from "./gilded-rose-functions";
 import { Item } from "./item";
 
 interface ItemTypes {
@@ -12,44 +13,7 @@ const ItemTypes: ItemTypes = {
   S_HAND: 'Sulfuras, Hand of Ragnaros'
 }
 
-const updateABrie = (item: Item) => {
-  if (item.quality < 50) {
-    item.quality = item.quality + 1;
-  }
-  item.sellIn = item.sellIn - 1;
-  if (item.sellIn < 0 && item.quality < 50) {
-    item.quality = item.quality + 1;
-  }
-}
 
-const updateBPass = (item: Item) => {
-  if (item.quality < 50) {
-    item.quality = item.quality + 1
-    if (item.sellIn < 11 && item.quality < 50) {
-      item.quality = item.quality + 1;
-    }
-    if (item.sellIn < 6 && item.quality < 50) {
-      item.quality = item.quality + 1;
-    }
-  }
-  item.sellIn = item.sellIn - 1;
-  if (item.sellIn < 0) {
-    item.quality = item.quality - item.quality;
-  }
-}
-
-const updateSHand = (item: Item) => {
-}
-
-const updateDefault = (item: Item) => {
-  if (item.quality > 0) {
-    item.quality = item.quality - 1;
-  }
-  item.sellIn = item.sellIn - 1;
-  if (item.sellIn < 0 && item.quality > 0) {
-    item.quality = item.quality - 1;
-  }
-}
 
 export class GildedRose {
   items: Array<Item>;
