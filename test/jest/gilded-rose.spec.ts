fcac99f1d6802df01fee5e74ab842a94794338ca
Mustafa Yilmaz <mustafa.yilmaz@kobil.com>
2022-07-28 02:41:04 +0300

    Add: conjured items jest and mocha test cases

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -101,3 +101,37 @@ describe('sulfuras quality rules', () => {
     expect(added.sellIn).toBe(1);
   });
 })
+
+describe('conjured items', () => {
+  it('should update quality for conjured sellin 1 day', () => {
+    const gildedRose = new GildedRose([new Item('Conjured', 1, 2)]);
+    const items = gildedRose.updateQuality();
+    const added = items[0]
+    expect(added.quality).toBe(0);
+    expect(added.sellIn).toBe(0);
+  });
+
+  it('should update conjured quality 4x as fast for sellin 0 days', () => {
+    const gildedRose = new GildedRose([new Item('Conjured', 0, 4)]);
+    const items = gildedRose.updateQuality();
+    const added = items[0]
+    expect(added.quality).toBe(0);
+    expect(added.sellIn).toBe(-1);
+  });
+
+  it('conjured item quality should never go below 0', () => {
+    const gildedRose = new GildedRose([new Item('Conjured', 0, 1)]);
+    const items = gildedRose.updateQuality();
+    const added = items[0]
+    expect(added.quality).toBe(0);
+    expect(added.sellIn).toBe(-1);
+  });
+
+  it('should lower quality by 3 if exactly 5 days left', () => {
+    const gildedRose = new GildedRose([new Item('Conjured', 5, 6)])
+    const items = gildedRose.updateQuality();
+    const added = items[0]
+    expect(added.quality).toBe(3);
+    expect(added.sellIn).toBe(4);
+  })
+})
